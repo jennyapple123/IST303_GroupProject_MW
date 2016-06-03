9b89fb74b8576ea5273c61029b9e382107060ff3
jennyapple123 <wenjiezhang01@gmail.com>
2016-06-03 13:40:02 -0700

    Update IST303Project.py

--- IST303Project.py
+++ IST303Project.py
@@ -1,4 +1,4 @@
-from Tkinter import *
+#from Tkinter import *
 #root = Tk()
 #root.mainloop()
 from datetime import *
@@ -9,8 +9,7 @@ import unittest
 
 
 guest_list = pd.read_csv("IST 303 GuestList.csv", index_col = 0)
-service_list = pd.read_csv("IST 303 services.csv", index_col = 0)
-
+#service_list = pd.read_csv("IST 303 services.csv", index_col = 0)
 
 
 class customer:
@@ -23,7 +22,7 @@ class customer:
         """
         for lab, row in guest_list.iterrows():
             if self.input_id == lab:
-                print str(self.input_id) + " is a valid ID"
+                #print str(self.input_id) + " is a valid ID"
                 self.check_in_date = row['check_in_date']
                 self.check_out_date = row['check_out_date']
                 self.check_in_time = row['check_in_time']
@@ -31,81 +30,70 @@ class customer:
                 self.check_in = datetime.strptime(row['check_in_date'] + " " + row['check_in_time'], '%m/%d/%y %I:%M %p')
                 self.check_out = datetime.strptime(row['check_out_date'] + " " + row['check_out_time'], '%m/%d/%y %I:%M %p')
                 return True
-        print str(self.input_id) + " is NOT a valid ID"
+        #print str(self.input_id) + " is NOT a valid ID"
         return False
 
-    def validate_service_CheckinCheckout(self, Input_Date, Input_Time):
-        """ This function checks if Input_Time is in the range of check in and check out range.
-        """
-        if self.check_for_cust():
-            ServiceTime = datetime.strptime(Input_Date + " " +  Input_Time, '%m/%d/%y %I:%M %p')
-            if ServiceTime > self.check_in and ServiceTime < self.check_out:
-                print Input_Date + " " +  Input_Time + " is in the range of check in and check out time"
-                return True
-            print Input_Date + " " +  Input_Time + " is NOT in the range of check in and check out time"
-            return False
+    
+Reservations = []
+
+class Service(customer):
+    def __init__(self, input_id):
+        self.input_id = input_id
+
+    def reserve(self, Input_Date, Input_Time, length):
+        self.StartTime = datetime.strptime(Input_Date + " " +  Input_Time, '%m/%d/%y %I:%M %p')
+        self.length = length
+        self.EndTime = self.StartTime + timedelta(minutes = length)
+        Reservations.append(self)
+
+class Facial_norm(Service):
+    def __init__(self, input_id):
+        self.input_id = input_id
+        self.service_id = 0
+
+class Facial_col(Service):
+    def __init__(self, input_id):
+        self.input_id = input_id
+        self.service_id = 1
 
 
-    def validate_service(self, Input_Date, Input_Time, Input_Service):
-        """ This function validate whether there are duplicate reservations.
-            It also checks whether the reservation is available.
-        """
-        if self.check_for_cust() and self.validate_service_CheckinCheckout(Input_Date, Input_Time):
-            for lab1, row1 in service_list.iterrows():
-                if row1['StartTime'] == Input_Time and row1['Date'] == Input_Date and row1['Services'] == Input_Service:
-                    if row1['ID']== 999999:
-                        print Input_Service + " on " + Input_Date + " " +  Input_Time + " is available. Would you like to book it?"
-                        #customer.start_reserving(self, Input_Time, Input_Service)
-                        return True
-                    else:
-                        if self.input_id == int(row1['ID']):
-                        #print row1['StartTime'],row1['Services']
-                            print "You ( ID = " + str(self.input_id) + ") already have booked that time slot"
-                            return False
-                        else:
-                            print "Someone else has booked that time slot!"
-                            return False
-            print "There is NO such service type or time slots. Please check your spelling!!"
-            return False
-        return False
-
-    def start_reserving(self, Input_Date, Input_Time, Input_Service):
-        """ This function helps to reserve a certain time slot for a certain type of service.
-        """
-        for lab1, row1 in service_list.iterrows():
-            if row1['StartTime'] == Input_Time and row1['Date'] == Input_Date and row1['Services'] == Input_Service:
-                if row1['ID']== 999999:
-                    service_list.set_value(lab1, 'ID' , self.input_id)
-                    print "Reservation for " + Input_Service + " on " + Input_Date + " " +  Input_Time + " is completed!"
-                    return
-
-    def lookup_service(self):
-        """ This function lists all the reservations made for a given customer
-        """
-        for lab, row in guest_list.iterrows():
-            if self.input_id == lab:
-                print "Customer ID is valid. Here are the reservation info"
-                print row
-                for lab1, row1 in service_list.iterrows():
-                    if self.input_id == int(row1['ID']):
-                        print row1
-                        print "\n"
-                print "No reservation yet"
-                return
-        print "Customer ID is not valid"
+class AvailableService():
+    def __init__(self, Input_Date, Input_Time, OpenTime, CloseTime):
+        self.ReserveDateTime = datetime.strptime(Input_Date + " " +  Input_Time, '%m/%d/%y %I:%M %p')
+        self.ReserveTime = datetime.strptime(Input_Time, '%I:%M %p')
+        self.OpenTime = datetime.strptime("8:00 AM", '%I:%M %p')
+        self.CloseTime = datetime.strptime("8:00 PM", '%I:%M %p')
+    
+    def Check(self):
+        ServiceList = ["Facial Norm", "Facial Collagen"]
+        NotAvailable = []
+        if self.ReserveTime > self.CloseTime or self.ReserveTime < self.OpenTime:
+            print("Not in the range of OpenTime and CloseTime")
+            return 
+    
+        for EachResevation in Reservations:
+            if EachResevation.StartTime - timedelta(minutes = EachResevation.length) < self.ReserveDateTime or self.ReserveDateTime < EachResevation.EndTime:
+                NotAvailable.append(EachResevation.service_id)
+        print(NotAvailable)
+        for i in range(0, 2, 1):
+            if i not in NotAvailable:
+                print(ServiceList[i])
 
 
 
 
-############ Object reservations (to be used in iteration2)
-"""
-class reservations(customer):
-    def __init__(self, mineral_bath, massage, facial, specialty):
-        self.mineral_bath = mineral_bath
-        self.massage = massage
-        self.facial = facial
-        self.specialty = specialty
-"""
+
+CustomerInstance = customer(102)
+FN1 = Facial_norm(102)
+FN1.reserve("5/22/16", "8:00 AM", 60)
+print(Reservations[0].EndTime)
+
+AS = AvailableService("5/22/16", "8:00 AM", "8:00 AM", "8:00 PM")
+AS.Check()
+
+#print "\n Cusotmer102 8:00 AM Massage"
+#CustomerInstance.validate_service("8:00 AM", "Massage")
+
 
 ############ Interactive inputs
 """
